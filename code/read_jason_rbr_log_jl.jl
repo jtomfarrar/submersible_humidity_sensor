7e4bbb6c837e6d9f071c860de82c11463719cfdb
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2022-11-09 16:23:47 -0800

    updated .jl %%-script

--- code/read_jason_rbr_log_jl.jl
+++ code/read_jason_rbr_log_jl.jl
@@ -91,10 +91,17 @@ dt2min(dt) = (dt-dt[1])./Minute(1.) # convert time to number in minutes
 
 
 # %%
+# load Rotronics data from file
 rot_df = Dict( "Date" => dateformat"m/d/y", "Time" => dateformat"I:M:S p" )
-rot = CSV.File( joinpath(datapath, "rot_0020568591_20220905_114308.csv"), header=41, skipto=45, drop=[5,], dateformat=rot_df )
+rot = CSV.File( joinpath(datapath, "rot_0020568591_20220905_114308.csv"),
+                header=41,
+                skipto=45,
+                drop=[5,],
+                dateformat=rot_df )
 
 # %%
+# load RBR temperature sensor data and plot on one axis
+
 filenames = joinpath.( datapath, 
                 ["603_05Sep22.log", "604_05Sep22.log", "605_05Sep22.log"] )
 
@@ -122,10 +129,7 @@ legend(["603", "604", "605"]; frameon=false)
 
 # %%
 ax = subplot(1,1,1)
-# for probe = 1:3
-#     dtime, msec, temp = get_data( filenames[probe] )
-#     plot( dtime, temp )
-# end
+
 plot( dtime603, t603 )
 plot( dtime604, t604 )
 plot( dtime605, t605 )
@@ -150,6 +154,10 @@ legend(["603 fat temp probe", "604 skinny probe", "605 skinny probe with stainle
 # Take troughs as the ventilated times when we approach the T and Twet (e.g. between  13:20-13:35)
 #
 
+# %%
+#Take troughs 13:20-13:35 as the ventilated times when we approach the T and Twet.
+# 604 T, 605 Twet
+
 # %% [markdown]
 # # Psychrometry
 
@@ -164,6 +172,9 @@ L0 = 2.501e6 # J/kg
 Rd = 287.04
 Rv = 461.5
 RdoRv=Rd/Rv
+
+"latent heat of water vapor"
+LvK(TempK) = 2.501e6 + (Cpv-Cw) * (TempK-273.0)
 
 """
 es(T,p) = is saturation vapor pressure based on Wexler's formula,
@@ -203,7 +214,7 @@ p[Pa], T[degrees C]
 273, 273.15 shifts in Temp are deliberate, to offset other
 inconsistencies in the thermo functions. SPdeS 01.July.2005
 """
-dqsdT(p,Temp) = qs(p,Temp)*Lv(Temp+273)/(Rv*(Temp+273.15)^2)
+dqsdT(p,Temp) = qs(p,Temp)*LvK(Temp+273)/(Rv*(Temp+273.15)^2)
 
 """
 Tw=Twet(Tdry[C], qv[kg/kg], p[Pa])
@@ -220,7 +231,7 @@ qs0 = qs(p,Tw)
 qw = min(qs0, qv)
 
 Lvap = 2.501e6 + (Cpv-Cw) * Tw
-stepfac = nanmean(Cp/(Lvap*dqsdT(p, Tw))) # ~0.3
+stepfac = Cp/(Lvap*dqsdT(p, Tw)) # ~0.3
 
 # Steps by 1/4 of saturation deficit and enforces -Cp*dT=L*dq by successive approximations
 for iter = 1 : niter
@@ -258,21 +269,56 @@ return q
 end
 
 # %%
-# set up interpolation
+# time interpolations
+
 uniqueidx(v) = unique(i -> v[i], eachindex(v))
-knots = Dates.value.(dtime603)
-ui = uniqueidx(knots)
 
-itp = LinearInterpolation(knots[ui], t603[ui])
-t603_605 = itp( Dates.value.(dtime605) )
-# itp = interpolate( (Dates.value.(dtime603),), t603, BSpline(Linear()) )
+"return interpolator to map T(x) to T(y) at knots y"
+function interpx2y(x::Vector, T::Vector)
+    ui = uniqueidx(x)
+    itp = LinearInterpolation(x[ui], T[ui])
+end
 
-q = q_psychro.(t603_605, t605, 101300.0)
-qtest = q_psychro.(t603_605.-0.5, t605.-0.5, 101300.0)
+dv(x::DateTime) = Dates.value(x)
+itp_to_605(dt, T) = interpx2y(dv.(dt), T)(dv.(dtime605))
 
-qw = qs.(101300.0, t603_605)
+t603_605 = itp_to_605(dtime603, t603)
+t604_605 = itp_to_605(dtime604, t604)
 
-rot_q = rot.Humidity/100 .* qs.(101500, rot.Temperature) # kg/kg
+# test fcns w/ imperative example
+# knots = Dates.value.(dtime603)
+# ui = uniqueidx(knots)
+# itp = LinearInterpolation(knots[ui], t603[ui])
+# t603_605 == itp( Dates.value.(dtime605) ) # true -- passes
+
+
+# %%
+# do psychrometry
+
+p0 = 101500.0 # nominal pressure, Pa
+q = q_psychro.(t603_605, t605, p0)
+
+# compute test humidity with temperature offsets
+qtest = q_psychro.(t603_605.-0.5, t605.-0.5, p0)
+# qtest = q_psychro.(t603_605.-1.5, t605.-1.0, p0) # also plausible
+
+# we really need an offset for t604 and t605
+qtest604 = q_psychro.(t604_605.-1.0, t605.-1.0, p0)
+
+qw = qs.(p0, t603_605)
+
+rot_q = rot.Humidity/100 .* qs.(p0, rot.Temperature) # kg/kg
+rot_tw = Twet.(rot.Temperature, rot_q, 101500.0)
+
+# interpolate rotronics T and Tw to 605 time
+rot_dt = Date(1,1,1) .+ rot.Time + Second(60*7.5)
+# rot_dv = dv.( rot_dt ) # datevalue
+t_rot_605 =  itp_to_605(rot_dt, rot.Temperature)
+tw_rot_605 = itp_to_605(rot_dt, rot_tw)
+
+# %%
+
+# plot q, qs calculations
 
 nanmask(f, x) = f(x) ? x : NaN
 # how do I broadcast it?
@@ -284,14 +330,33 @@ plot(dtime605, mask.+q )
 plot(dtime605, mask.+qw)
 
 plot( Date(1,1,1) .+ rot.Time + Second(60*7.5) , rot_q )
-# tw = Twet(t603_605, q, 101300.0)
+# tw = Twet(t603_605, q, p0)
 plot(dtime605, mask.+qtest)
+plot(dtime605, mask.+qtest604)
 
 xlim([DateTime(1,1,1,13,0,0), DateTime(1,1,1,13,40,0)])
 X = ax.xaxis
 mdates = PyPlot.matplotlib.dates
 X.set_major_locator(mdates.MinuteLocator(10))
 X.set_major_formatter(mdates.DateFormatter("%H:%M")) # Specify the format - %b gives us Jan, Feb...
-legend(["q psychrometer", "qs", "q Rotronics"]; frameon=false)
+legend(["q psychrometer", "qs", "q Rotronics",
+        "T603 -0.5", "T604 -1.0"]; frameon=false)
 
 # %%
+# plot T, Tw from 604-605 thermistor calculations
+# compared to T, Tw from Rotronics
+subplot(1,1,1)
+plot(dtime605, t604_605)
+plot(dtime605, t605)
+plot(dtime605, t_rot_605 )
+plot(dtime605, tw_rot_605 )
+
+xlim([DateTime(1,1,1,13,0,0), DateTime(1,1,1,13,40,0)])
+X = ax.xaxis
+mdates = PyPlot.matplotlib.dates
+X.set_major_locator(mdates.MinuteLocator(10))
+X.set_major_formatter(mdates.DateFormatter("%H:%M")) # Specify the format - %b gives us Jan, Feb...
+legend(["T604", "T605 (wet)", "Rotronics T","Rotronics Tw"]; frameon=false)
+
+# %% [markdown]
+# Even troughs in T604, T605 (wet) during 13:15-13:30 are warmer than the T, Tw independently measured by Rotronics.
