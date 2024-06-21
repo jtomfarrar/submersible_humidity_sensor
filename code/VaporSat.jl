f8838b4f33cc849f337ad02e45541aad6779c28c
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2024-06-21 08:52:31 -0700

    read SHS temperature and temperature1, compare to Jason's T bridge

---
+++ code/VaporSat.jl
@@ -0,0 +1,80 @@
+module VaporSat
+
+using ForwardDiff
+
+export qs, dqsdT
+export Twet_autodiff
+
+# constants
+Cp = 1005.7  # from my Davies-Jones function, was 1005.
+Cpv = 1870.0 # J/kg/K
+Cw  = 4190.0
+L0 = 2.501e6 # J/kg
+
+C = 273.15 # K
+Rd = 287.04
+Rv = 461.5
+RdoRv=Rd/Rv
+
+"latent heat of water vapor"
+LvK(TempK) = L0 + (Cpv-Cw) * (TempK-273.0)
+
+# functions
+"""
+es(T,p) = is saturation vapor pressure based on Wexler's formula,
+with enhancement factor for moist air rather than water vapor.
+The enhancement factor requires a pressure.
+T [degrees C], p [Pa] (note the reversed input order), es [Pa]
+Calling with optional keywords changes the units and
+ignores the positional arguments.
+es(T,p; TK=tk[Kelvin], P=pr[hPa])
+From A. L. Buck 1981: JAM, 20, 1527-1532.
+SPdeS 7 July 2004
+"""
+function es(T,p=1e5; P=p*1e-2)
+    esat = 1e2 * 6.1121*(1.0007 + 3.46e-8*P)*exp((17.502*T)/(240.97 + T)) # convert es to Pa
+end
+
+# supply TK [Kelvin] by keyword, ignores positional T!!
+function es(T,p=1e5; TK=T+C, P=p*1e-2)
+    T = TK - C
+    esat = 1e2 * 6.1121*(1.0007 + 3.46e-8*P)*exp((17.502*T)/(240.97 + T)) # convert es to Pa
+end
+
+"""
+qs(p,T) is saturation specific humidity based on Wexler's formula for es
+with enhancement factor (see es.m).
+p [Pa], T [degrees C], qs [kg/kg]
+From A. L. Buck 1981: JAM, 20, 1527-1532.
+SPdeS 7 July 2004
+"""
+qs(p,T) = RdoRv*es(T,p) / (p + (RdoRv-1)*es(T,p))
+
+"dqsdT(p,T[C]) derivative of qs with respect to T at p,T by autodiff of Bolton's qs"
+dqsdT(p,T) = ForwardDiff.derivative(t -> qs(p,t), T)
+
+# wet bulb temperature methods
+# for approximating the evap process
+
+"General single Newton iteration to update x toward f(x) = fhat for a univariate function f"
+updatex(f, x, fhat) = x + (fhat-f(x)) / ForwardDiff.derivative(f, x)
+
+"""
+Twet_autodiff(T[K], q[kg/kg], p[Pa]; niter=2) wet bulb temperature using Newton's method
+for target specific humidity q[kg/kg]. Uses automatic differntiation.
+"""
+function Twet_autodiff(T, q, p; niter=2)
+    f(t) = (t - T) + LvK((T+t)/2)/Cp * (qs(p,t-C) - q)
+    t=T
+    for i in 1:niter
+        t = updatex(f, t, 0)
+    end
+    t
+end
+# 2 iterations converges to ~0.001 K
+
+# call as...
+# q = rh*qs(pa, Ta)
+# Twet_autodiff(Ta, rh*qs(pa, Ta-C), pa) 
+
+end # module VaporSat
